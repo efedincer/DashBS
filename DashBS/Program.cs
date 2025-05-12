b2f7144f657f9cb9ee77b65c8b579f2d1ea14f7f
efedincer <dincer.efe@gmail.com>
2025-05-12 16:15:41 +0300

    added some styles and AdminController

--- DashBS/Program.cs
+++ DashBS/Program.cs
@@ -22,7 +22,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Admin}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
