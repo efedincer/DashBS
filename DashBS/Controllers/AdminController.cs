6e225cb72e46165945f876184b8ded8d7ed1e65e
efedincer <dincer.efe@gmail.com>
2025-05-12 16:33:12 +0300

    layout bit changed

--- DashBS/Controllers/AdminController.cs
+++ DashBS/Controllers/AdminController.cs
@@ -3,8 +3,33 @@
 namespace DashBS.Controllers;
 public class AdminController : Controller
 {
-    public IActionResult Index()
-    {
-        return View();
-    }
+    public IActionResult Index() => View();
+
+    public IActionResult Dashboard() => View();
+
+    public IActionResult Users() => View();
+
+    public IActionResult Settings() => View();
+
+    public IActionResult Details() => View();
+
+    public IActionResult About() => View();
+
+    public IActionResult Contact() => View();
+
+    public IActionResult Error() => View();
+
+    public IActionResult OnGet() => View();
+
+    public IActionResult OnPost() => View();
+
+    public IActionResult OnPut() => View();
+
+    public IActionResult OnDelete() => View();
+
+    public IActionResult OnPre() => View();
+
+
+
+
 }
